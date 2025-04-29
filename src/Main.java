6af85430c95c5640267458b9bf754833592132e8
Aleksei Stukalov <stukalov.nz@gmail.com>
2025-04-29 14:59:25 +1000

    add displayInfor function with general information output

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
 // Aleksei Stukalov
 // 25016435
-// https://github.com/aleksei-stukalov/159234-assignment-1
+// https://github.com/aleksei-stukalov/159234-assignment-2
 
 public class Main {
+    /**
+     * Displays the general assignment information including student details
+     * and major.
+     */
+    private static void displayInfo() {
+        System.out.println("**************************************");
+        System.out.println("Assignment 2, 159.234 Semester 1 2025");
+        System.out.println("Submitted by: Aleksei Stukalov, ID: 25016435");
+        System.out.println("My Major at Massey: Software Engineering");
+        System.out.println("**************************************");
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        displayInfo();
     }
 }

8f0f4d93da02ed2cb250be1607a00b90d406ab60
Aleksei Stukalov <stukalov.nz@gmail.com>
2025-04-29 14:51:56 +1000

    initial code

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+// Aleksei Stukalov
+// 25016435
+// https://github.com/aleksei-stukalov/159234-assignment-1
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
